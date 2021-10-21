2518a2a7f2d2a174228aadff3a0d03b4603e5423
Ocay <john.alex.ocay@accenture.com>
2021-10-21 23:13:16 +0800

    Commit

--- src/main/java/com/accenture/ws/controller/OrderAndBillingController.java
+++ src/main/java/com/accenture/ws/controller/OrderAndBillingController.java
@@ -26,7 +26,7 @@ public class OrderAndBillingController {
 	private OrderRepository orderRepo;
 	
 	private CafeClerk clerk;
-	private static final String CONST_URL = "http://localhost:8080/kopeetearia-angular/api";
+	private static final String CONST_URL = "http://localhost:4200";
 
 	@GetMapping(value = "/orders")
 	@CrossOrigin(origins = CONST_URL)
